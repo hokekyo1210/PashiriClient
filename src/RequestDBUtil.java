6f023ce9549bced3754a3cfdddaec8ff693dff54
ETTSU <34941162+ETTSU@users.noreply.github.com>
2017-12-30 18:38:05 +0900

    依頼期限設定メソッドの追加
    setDeadlineメソッドをRequestDBUtilに追加した。
    Date型がjava.util.Dateなのかjava.sql.Dateなのか、
    依頼班やUI班の希望する日時の表示形式、渡す値の形式で変わってくるが暫定として作成。

--- src/RequestDBUtil.java
+++ src/RequestDBUtil.java
@@ -1,4 +1,40 @@
 
 public class RequestDBUtil {
+	/**
+	 * <p>setDeadlineメソッドは、依頼IDに該当する依頼の期限を設定（更新）する。</p>
+	 *
+	 * @author 5402 越後 優太
+	 *
+	 * @param requestId 設定目標の依頼ID
+	 * @param deadline 設定する期限
+	 * @return 期限設定の成否
+	 */
+	public static boolean setDeadline( int requestId, Date deadline )	{
+		// java.util.Date型からdatetime型の形式「yyyy-MM-dd HH:mm:ss」に整形
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String convDeadline = sdFormat.format( deadline );
 
+		String query = "UPDATE Requests SET Deadline = cast('" + convDeadline + "' as Datetime ) WHERE RequestID = " + requestId;	//SQL文を生成
+
+		try	{
+		        SQLManager.requestDBUpdate( query );	//SQLManagerのメソッドを用いてクエリを飛ばす(この場合は依頼データベースに)
+		}	catch (Exception e)	{
+			      e.printStackTrace();
+			      return false;	//クエリ送信失敗
+		}
+		return true;	//期限設定成功
+
+		// 変換不要の場合(String型でdatetime型の形式「yyyy-MM-dd HH:mm:ss」で渡される)
+		/*
+		String query = "UPDATE Requests SET deadline = " + deadline + " WHERE RequestID = " + requestId;	//SQL文を生成
+		System.out.println( query );
+		try	{
+			      ResultSet resultSet = SQLManager.requestDBQuery( query );	//SQLManagerのメソッドを用いてクエリを飛ばす(この場合は依頼データベースに)
+		}	catch (Exception e)	{
+			      e.printStackTrace();
+			      return false;	//クエリ送信失敗
+		}
+		return true;	//期限設定成功
+		*/
+	}
 }
